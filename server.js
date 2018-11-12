a8759676e1dadcc23d509579f2b11dfc7a82c8de
Josie <joseffa.steuernagel@haw-hamburg.de>
2018-11-12 12:05:24 +0100

    sessionvariable
    Anmelden per login auf Sessionvariable ist jetzt möglich. Yay

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 //Initialisiere Datenbanken
 const sqlite3 = require('sqlite3').verbose();
 
+const db = new sqlite3.Database('meetyourcity.db');
+
 // Express.js Webserver
 const express = require('express');
 const app = express()
@@ -34,47 +36,62 @@ app.listen(3000, function(){
 
 //---------------------------------------------//
 
-app.get('/erste_schritte', function(req, res) {
-  res.render('erste_schritte');
+//------------Sessionvariablen---------------//
+app.get('/', function(req, res) {
+	if(!req.session.authenticated) {
+		res.render('start-login');
+	}
+	else {
+		res.render('home', {
+			'username': req.session.user.name
+		});
+	}
 });
 
-//app.post('/erste_schritte', function(req, res) {
-//	const db = new sqlite3.Database()
-//	const {wohnort, interessen} = req.body;
+app.post('/sendLogin', function(req, res) {
+	//in Datenbank gucken
+	const user = req.body["username"];
+	const password = req.body["password"];
+	db.get(`SELECT * FROM users WHERE username='${user}'`, function(err, row) {
+		if (row != undefined) {
+			//Wenn ja, schau ob das Password richtig ist
+			if(password == row.password) {
+				//hat geklappt
+				//Sessionvariable setzen
+				req.session['user'] = user;
+				req.session.authenticated = true;
+				res.redirect('/home');
+			}else{
+				//hat nicht geklappt weil password falsch
+				res.redirect('/loginerror');
+			}
+		}else{
+			//hat es nicht geklappt, weil kein User mit dem Namen
+			res.redirect('/loginerror');
+		}
+		//Falls ein Fehler auftritt in der Abfrage, gebe ihn aus
+		if(err){
+			console.error(err.message);
+		}
+	});
+});
 
-//	db.run(`INSERT INTO profilinfo()`)
-//});
+app.get('/logout', function(req, res){
+	delete req.session['user'];
+	res.redirect('/start-login');
+});
+//==========================================//
 
-//app.post('/registrierung', function(req, res) {
-//	const db = new sqlite3.Database('meetyourcity.db');
-//	const { email, password, username } = req.body;
-		// validierung
-//	db.run(`INSERT INTO users(email,password,username) VALUES(?, ?, ?)`, [email, password, username], function(err) {
-//		 if (err) {
-//			 return console.log(err.message);
-//		 }
-//		 return res.redirect('/erste_schritte')
-//	 });
-//	 db.close();
-//});
-//============================================//
+app.get('/start-login', function(req, res) {
+	res.render('start-login');
+});
+
 app.get('/home', function(req, res) {
-  res.render('home');
+	res.render('home');
 });
 
 app.get('/loginerror', function(req, res) {
-  res.render('loginerror');
-});
-
-app.get('/neue_Veranstaltung', function(req, res) {
-  res.render('neue_Veranstaltung');
-});
-
-app.get('/profil_bearbeiten', function(req, res) {
-  res.render('profil_bearbeiten');
-});
-app.get('/profil', function(req, res) {
-  res.render('profil');
+	res.render('loginerror');
 });
 
 //----------DB Registrierung--------------//
@@ -84,55 +101,19 @@ app.get('/registrierung', function(req, res) {
 });
 
 app.post('/registrierung', function(req, res) {
-	const db = new sqlite3.Database('meetyourcity.db');
 	const { email, password, username } = req.body;
 		// validierung
 	db.run(`INSERT INTO users(email,password,username) VALUES(?, ?, ?)`, [email, password, username], function(err) {
 		 if (err) {
 			 return console.log(err.message);
 		 }
-		 return res.redirect('/erste_schritte')
+		 return res.redirect('/erste_schritte');
 	 });
-	 db.close();
 });
 
-//app.all('/registrierung', function(req, res) {
-//	const db = new sqlite3.Database('meetyourcity.db');
-//	const { email, password, username } = req.body;
-	// validierung
-//	db.run(`INSERT INTO users(email,password,username) VALUES(?, ?, ?)`, [email, password, username], function(err) {
-//		 if (err) {
-//			 return console.log(err.message);
-//		 }
-//	 });
-//   res.render('registrierung');
-//	 db.close();
-//});
 
 //=======================================//
-
-//----------DB Login---------------------//
-
-app.get('/start-login', function(req, res) {
-  res.render('start-login');
-});
-
-app.post('/start-login', function(req, res) {
-	const db = new sqlite3.Database('meetyourcity.db');
-	db.get(`SELECT * FROM users WHERE username=? AND password=?`, [req.body.username, req.body.password], function(err, row) {
-		 if (err) {
-			 return console.log(err.message);
-		 }
-		 if (row) {
-			 return res.redirect('/home')
-		 }
-		 return res.redirect('/loginerror')
-	 });
-	 db.close();
-});
-//=========================================//
-
-
-app.get('/veranstaltung_unterseite', function(req, res) {
-  res.render('veranstaltung_unterseite');
+//Called when a URL is called that is not implemented
+app.use((request, response, next) => {
+	response.status(404).render('error');
 });
