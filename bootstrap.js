e2c5ca7d6ffcf77699c05f238e9a6c09981d2306
Josie <joseffa.steuernagel@haw-hamburg.de>
2018-11-14 16:20:46 +0100

    BLOB-Tabelle

--- bootstrap.js
+++ bootstrap.js
@@ -9,5 +9,7 @@ db.exec('DROP TABLE IF EXISTS users');
 // Create Users Table
 db.exec('CREATE TABLE users(email text, password text, username text, wohnort text)');
 
+// Create pictures Table
+db.exec('CREATE TABLE pictures(document_id INTEGER PRIMARY KEY, mime_type TEXT NOT NULL, doc BLOB);')
 
 db.close();
